b8a9db78865823c17d9ab794959b1cd57b6e9763
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2021-08-17 23:02:28 +0200

    fix(schemas): updated types

--- prefab/prefab/schemas.js
+++ prefab/prefab/schemas.js
@@ -7,8 +7,8 @@ const prefabGuild = {
         default: PREFIX,
         type: String
     },
-    disabledCommands: Array,
-    disabledChannels: Array,
+    disabledCommands: [String],
+    disabledChannels: [String],
     commandPerms: {},
     commandCooldowns: {},
     commandAlias: {}
