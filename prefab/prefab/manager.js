1375f5497752bd44aa08886f115453f85777719d
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2021-08-17 22:54:52 +0200

    feat(manager): added additional optional params

--- prefab/prefab/manager.js
+++ prefab/prefab/manager.js
@@ -4,7 +4,7 @@ const { Collection } = require('discord.js');
 
 class Manager {
     /**
-     * @param {import('./client')} client 
+     * @param {import('../src/util/client')} client 
      * @param {import('mongoose').Model} model
      */
     constructor (client, model) {
@@ -13,7 +13,11 @@ class Manager {
         this._cache = new Collection();
     }
 
-    async get (key) {
+    async get (key, force) {
+        if (!key) return;
+
+        if (force) return this.findOne({ _id: key })
+
         let item = this._cache.get(key);
         if (!item) {
             item = await this._model.findOneAndUpdate({ _id: key }, {  }, { new: true, upsert: true, setDefaultsOnInsert: true });
@@ -33,13 +37,19 @@ class Manager {
 
     async findOne (filter) {
         const item = await this._model.findOne(filter);
+
+        if (!item) return;
+
         this._cache.set(item._id, item);
+
         return item;
     }
 
     async findMany (filter) {
         const items = await this._model.find(filter);
+
         for (const item of items) this._cache.set(item._id, item);
+
         return items;
     }
 
@@ -49,43 +59,67 @@ class Manager {
 
     async findOneAndUpdate (filter, update, options) {
         const item = await this._model.findOneAndUpdate(filter, update, options);
+
         this._cache.set(item._id, item);
+
         return item;
     }
 
     async updateMany (filter, update, options) {
         const query = await this._model.updateMany(filter, update, options);
+
         return query;
     }
 
-    async findByIdAndDelete (key) {
-        await this._model.deleteOne({ _id: key });
+    async findByIdAndDelete (key, options) {
+        if (!key) return;
+
+        await this._model.findByIdAndDelete(key, options);
+
         const item = this.cache.get(key);
+
         this._cache.delete(key);
+
         return item;
     }
 
-    async findOneAndDelete (filter) {
-        await this._model.deleteOne(filter);
+    async findOneAndDelete (filter, options) {
+        const item = await this._model.findOneAndDelete(filter, options);
+
+        if (!item) return;
+
+        this._cache.delete(item._id);
+
+        return item;
     }
 
-    async deleteMany (filter) {
-        await this._model.deleteMany(filter);
+    async deleteMany (filter, options) {
+        await this._model.deleteMany(filter, options);
     }
 
     async insertOne (item) {
+        if (!item) return;
+
         return (await this.insertMany([item]))[0];
     }
 
     async insertMany (items) {
+        if (!items || !items.length) return;
+
         const query = await this._model.insertMany(items);
+
         for (const item of query) this._cache.set(item._id, item);
+
         return query;
     }
 
     async exists (key) {
+        if (!key) return false;
+
         let item = this._cache.get(key);
-        if (!item) item = await this.findById(key);
+
+        if (!item) item = await this.findOne({ _id: key });
+
         return !!item;
     }
 
